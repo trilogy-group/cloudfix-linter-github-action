c25d9ac8485e034a05ace11648a3e2666ab5ead7
Yogesh Kumar <yogesh.kumar@trilogy.com>
2023-04-25 12:22:20 +0000

    wip

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@
 if [ "$ENABLE_MOCK_RECOMMENDATION" = "true" ]
 then
     export CLOUDFIX_FILE=true
+    echo $(terraform show -json)
     terraform show -json > tf.show
     mock_recco=$(python mainRepo/gen_recco.py tf.show)
     status=$?
